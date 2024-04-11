ae028f5efa8e3b4faf7fc2ed6c8c6bb793b444ec
Yakkaya <yakkaya@calpoly.edu>
2024-04-10 20:40:19 -0700

    step 4 complete!

--- packages/express-backend/src/backend.js
+++ packages/express-backend/src/backend.js
@@ -34,9 +34,22 @@ const users = {
 
 app.use(express.json());
 
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
 app.get("/users", (req, res) => {
+const name = req.query.name;
+if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+} else {
     res.send(users);
-  });
+}
+});
 
 app.listen(port, () => {
   console.log(
